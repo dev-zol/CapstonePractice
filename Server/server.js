eda496b390df0fef18aa18b96abc9983d6a001c1
dev-zol <tkrobotics13@gmail.com>
2021-10-22 15:10:59 -0500

    server setup finished

--- Server/server.js
+++ Server/server.js
@@ -1,22 +1,25 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
-
+const express = require("express");
+const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
-
+ 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
   res.json({ message: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 }); //Line
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
-
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
