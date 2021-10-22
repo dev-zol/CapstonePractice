205e2e1a43bdccfdc399d331a53c9e3a19f11571
dev-zol <tkrobotics13@gmail.com>
2021-10-22 14:46:12 -0500

    add connection files

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,15 @@ const express = require('express'); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 5000; //Line 3
 
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
   res.json({ message: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
